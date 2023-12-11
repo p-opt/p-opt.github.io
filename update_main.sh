03e27cef687be5e403275c480d41003ad8635028
pxxyyz <pxxyyz@gmail.com>
2023-12-11 16:56:36 +0800

    update

---
+++ update_main.sh
@@ -0,0 +1,23 @@
+python2 jemdoc.py index.jemdoc
+# python2 jemdoc.py paper.jemdoc
+# python2 jemdoc.py teaching.jemdoc
+# python2 jemdoc.py homework.jemdoc
+# python2 jemdoc.py lectures.jemdoc
+# python2 jemdoc.py reference.jemdoc
+# python2 jemdoc.py project.jemdoc
+
+# rsync -rvz www/*.html www/jemdoc.css yc5@nobel.princeton.edu:public_html/ele520_math_data/
+  
+# cp -R ~/Dropbox/UChicago/Courses/STAT37797/Lectures2021 Teaching/STAT37797/
+# cp -R ~/Dropbox/UChicago/Courses/STAT37797/Homework2021 Teaching/STAT37797/
+# cd Teaching/STAT37797
+# python2 jemdoc.py index.jemdoc
+# python2 jemdoc.py syllabus.jemdoc
+# python2 jemdoc.py course_info.jemdoc
+# python2 jemdoc.py homework.jemdoc
+# python2 jemdoc.py lectures.jemdoc
+# python2 jemdoc.py reference.jemdoc
+# python2 jemdoc.py project.jemdoc
+
+# rsync -rvz www/*.html www/jemdoc.css yc5@nobel.princeton.edu:public_html/ele520_math_data/
+  
